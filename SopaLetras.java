a8cc6ca95b90a498119b2fa8835f35f14785cb74
RubenS98 <A01021759@itesm.mx>
2018-01-17 19:32:01 -0600

    Tercera Clase Terminado

--- SopaLetras.java
+++ SopaLetras.java
@@ -1,36 +1,86 @@
-package javaapplication1;
-
+import java.util.Scanner;
 public class SopaLetras {
     public static void main (String[] args){
         String [][] bi = {{"M", "R", "D", "F", "O"},{"A", "R", "N", "T", "L"},{"N","O", "N", "A", "M"},
             {"O", "S", "D", "Y", "D"},{"J", "G", "H", "H", "L"}};
-        
+        Scanner lectura = new Scanner(System.in);
+        String palabra="";
+        int repeticiones=0;
+
         for(int r=0; r<bi.length; r++){
             for(int c=0; c<(bi[0].length); c++){
-                System.out.print(bi[r][c]+" ");      
+                System.out.print(bi[r][c]+" ");
             }
-            System.out.println(); 
+            System.out.println();
         }
-           
-        System.out.println("Palabra a buscar: MANO");
-        
-        apariciones(bi);
+
+        System.out.println("Escribe la palabra que quieres buscar en la sopa de letras (en mayusculas)");
+        palabra=lectura.next();
+
+        if(palabra.length()>bi.length){
+          System.out.println("Esa palabra no cabe en la sopa de letras. Elige otra.");
+          palabra=lectura.next();
+        }
+
         System.out.println();
-        
+        repeticiones=apariciones(bi, palabra);
+        System.out.println("La palabra "+palabra+" aparece "+repeticiones+" veces en la sopa de letras.");
+
     }//cierre de main
-    
-    public static int apariciones (String[][] bi){
+
+    public static int apariciones (String[][] bi, String palabra){
         int cont=0;
+        int cont2=0;
+        int largo=palabra.length();
         String sum="";
         String sum2="";
-        String palabra="MANO";
-        
-        for(int r=0; r<bi.length-palabra.length(); r++){
-            for(int c=0; c<(bi[0].length-palabra.length()); c++){
-                sum=bi[r][c]+bi[r][c+1]+bi[r][c+2]+bi[r][c+3];
+
+        for(int r=0; r<bi.length; r++){
+            for(int c=0; c<(bi[0].length-(largo-1)); c++){
+                while(cont2!=largo){
+                  sum=sum+bi[r][c+cont2];
+                  cont2++;
+                }
+                if(sum.equalsIgnoreCase(palabra)){
+                  cont++;
+                }
+                sum="";
+                cont2=largo-1;
+                while(cont2!=-1){
+                  sum2=sum2+bi[r][c+cont2];
+                  cont2--;
+                }
+                cont2=0;
+                if(sum2.equalsIgnoreCase(palabra)){
+                  cont++;
+                }
+                sum2="";
             }
         }
-        System.out.println();
+
+        for(int r=0; r<bi.length-(largo-1); r++){
+            for(int c=0; c<(bi[0].length); c++){
+                while(cont2!=largo){
+                  sum=sum+bi[r+cont2][c];
+                  cont2++;
+                }
+                if(sum.equalsIgnoreCase(palabra)){
+                  cont++;
+                }
+                sum="";
+                cont2=largo-1;
+                while(cont2!=-1){
+                  sum2=sum2+bi[r+cont2][c];
+                  cont2--;
+                }
+                cont2=0;
+                if(sum2.equalsIgnoreCase(palabra)){
+                  cont++;
+                }
+                sum2="";
+            }
+        }
+
         return cont;
     }
 }
